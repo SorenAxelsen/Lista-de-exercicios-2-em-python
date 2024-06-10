d833377a6dc255ff45f53de8acc2d4091d02d224
SorenAxelsen <163474970+SorenAxelsen@users.noreply.github.com>
2024-06-10 19:43:34 -0300

    atividade05.py

--- atividade05.py
+++ atividade05.py
@@ -1,16 +1,24 @@
-public class atividade05_1 {
-    protected double salario;
-    protected String departamento;
+def is_primo(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-    public atividade05_1(double salario, String departamento) {
-        this.salario = salario;
-        this.departamento = departamento;
-    }
+def main():
+    
+    vetor = []
+    for i in range(15):
+        num = int(input(f"Digite o {i+1}º número inteiro: "))
+        vetor.append(num)
 
-    public void exibirInformacoes() {
-        System.out.println("Salário: " + salario);
-        System.out.println("Departamento: " + departamento);
-    }
-}
+    
+    vetor_primos = [num for num in vetor if is_primo(num)]
 
+    
+    print("\nVetor resultante contendo apenas números primos:")
+    print(vetor_primos)
 
+if __name__ == "__main__":
+    main()
