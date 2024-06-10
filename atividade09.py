54c0ad4cc72a878c17f807dd2c1e3265fc1f0a26
SorenAxelsen <163474970+SorenAxelsen@users.noreply.github.com>
2024-06-10 19:44:49 -0300

    atividade09.py

--- atividade09.py
+++ atividade09.py
@@ -1,61 +1,13 @@
+import math
 
-interface Trabalhador {
-    void trabalhar();
-}
+def calcular_volume_esfera(raio):
+    volume = (4/3) * math.pi * (raio ** 3)
+    return volume
 
+def main():
+    raio = float(input("Digite o raio da esfera: "))
+    volume = calcular_volume_esfera(raio)
+    print("O volume da esfera é:", volume)
 
-class Funcionario implements Trabalhador {
-    protected double salario;
-    protected String departamento;
-
-    public Funcionario(double salario, String departamento) {
-        this.salario = salario;
-        this.departamento = departamento;
-    }
-
-    public void exibirInformacoes() {
-        System.out.println("Salário: " + salario);
-        System.out.println("Departamento: " + departamento);
-    }
-
-    @Override
-    public void trabalhar() {
-        System.out.println("Funcionário está trabalhando.");
-    }
-}
-
-
-class Gerente extends Funcionario {
-    private double bonus;
-
-    public Gerente(double salario, String departamento, double bonus) {
-        super(salario, departamento);
-        this.bonus = bonus;
-    }
-
-    @Override
-    public void trabalhar() {
-        System.out.println("Gerente está trabalhando. Bônus: " + bonus);
-    }
-}
-
-
-class Empresa {
-    public void adicionarTrabalhador(Trabalhador trabalhador) {
-        trabalhador.trabalhar();
-    }
-}
-
-public class atividade08 {
-    public static void main(String[] args) {
-        Empresa empresa = new Empresa();
-
-        
-        Funcionario funcionario = new Funcionario(3000, "TI");
-        Gerente gerente = new Gerente(5000, "Administração", 1000);
-
-        
-        empresa.adicionarTrabalhador(funcionario);
-        empresa.adicionarTrabalhador(gerente);
-    }
-}
+if __name__ == "__main__":
+    main()
